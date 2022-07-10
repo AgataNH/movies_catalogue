2d1387ed589c363bffb786384c8bbf87a89b0d76
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-07-10 21:45:26 +0200

    changes

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import tmdb_client
 
 app = Flask(__name__)
 
+list_type = ["now_playing", "upcoming", "top_rated", "popular"]
+
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get('list_type', "popular")
+
+    if selected_list not in list_type:
+        selected_list = "popular"
+
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list)
 
 @app.context_processor
 def utility_processor():
--- tmdb_client.py
+++ tmdb_client.py
@@ -10,12 +10,21 @@ def get_popular_movies():
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}?api_key=c2c05e38fdd57b4ae0276dfa0bd8d60c"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
     return data["results"][:how_many]
 
 def get_single_movie(movie_id):
