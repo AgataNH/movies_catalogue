0c9ce153ab90fa7f8f19051b0983b90e2c21a774
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-07-19 21:37:07 +0200

    add prtsc_test

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -3,19 +3,23 @@ from unittest.mock import Mock
 
 def test_get_single_movie(monkeypatch):
     mock_movie = ['Movie 1']
+    
     requests_mock = Mock()
     response = requests_mock.return_value
     response.json.return_value = mock_movie
     monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+    
     movies_list = tmdb_client.get_single_movie(movie_id = 1)
     assert movies_list == mock_movie
 
 def test_get_single_movie_cast(monkeypatch):
-    mock_cast = ['Cast 1']
+    mock_cast = ['Cast']
+
     requests_mock = Mock()
     response = requests_mock.return_value
     response.json.return_value = mock_cast
     monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+
     movie_cast = tmdb_client.get_single_movie_cast(movie_id = 1)
     assert movie_cast == mock_cast
 
