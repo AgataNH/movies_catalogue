f5e61f9f4cad40fc923ed6bd822a73cdb69abd4a
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-07-21 14:16:42 +0200

    change test_flask

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -11,16 +11,17 @@ def test_homepage(monkeypatch):
        assert response.status_code == 200
        api_mock.assert_called_once_with('movie/popular?api_key=c2c05e38fdd57b4ae0276dfa0bd8d60c')
 
-@pytest.mark.parametrize('endpoint, result', (
-    ('movie/popular?api_key=c2c05e38fdd57b4ae0276dfa0bd8d60c', []),
-    ('movie/now_playing?api_key=c2c05e38fdd57b4ae0276dfa0bd8d60c', []),
-    ('movie/upcoming?api_key=c2c05e38fdd57b4ae0276dfa0bd8d60c', []),
-    ('movie/top_rated?api_key=c2c05e38fdd57b4ae0276dfa0bd8d60c', [])
+@pytest.mark.parametrize('list_type', (
+    'popular',
+    'now_playing',
+    'upcoming',
+    'top_rated'
 ))
-def test_list_type(endpoint, result):
-    assert tmbd_client.call_tmbd_api(endpoint) == result
+def test_list_type(monkeypatch, list_type):
+    api_mock = Mock(return_value={'results': []})
+    monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
 
     with app.test_client() as client:
-       response = client.get('/{endpoint}')
+       response = client.get('/?list_type={list_type}')
        assert response.status_code == 200
-       api_mock.assert_called_once_with(endpoint)
+       api_mock.assert_called_once_with('movie/{list_type}?api_key=c2c05e38fdd57b4ae0276dfa0bd8d60c')
